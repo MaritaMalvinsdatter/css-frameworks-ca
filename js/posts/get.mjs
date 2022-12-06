6513ac3ccf7295118ef94f727d81cd7a175cc44d
MaritaMalvinsdatter <79151543+MaritaMalvinsdatter@users.noreply.github.com>
2022-12-06 14:44:29 +0100

    can get post(s)

---
+++ js/posts/get.mjs
@@ -0,0 +1,29 @@
+import { API_SOCIAL_URL } from "/js/api/constants.mjs";
+import { tokenFetch } from "/js/api/tokenFetch.mjs";
+const action = "/posts";
+
+// gets single post, id needed
+export async function getPost(id) {
+    if (!id) {
+        console.error("postID needed to get post");
+    }
+    const getPostURL = `${API_SOCIAL_URL}${action}/${id}`;
+    const response = await tokenFetch(getPostURL)
+    const post = await response.json();
+
+    // console.log(post);
+
+    return post;
+}
+
+
+// gets multiple posts
+export async function getPosts() {
+    const updatePostURL = `${API_SOCIAL_URL}${action}`;
+    const response = await tokenFetch(updatePostURL)
+    const post = await response.json();
+
+    // console.log(post);
+
+    return post;
+}
