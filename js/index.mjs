a18c7c3c0a9cda357a1c23ad289f724f5ab8b009
MaritaMalvinsdatter <79151543+MaritaMalvinsdatter@users.noreply.github.com>
2023-03-09 09:42:57 +0100

    filter and search

--- js/index.mjs
+++ js/index.mjs
@@ -1,19 +1,21 @@
 import * as handlers from "./eventHandlers/handlerIndex.mjs";
 import * as postMethods from "./posts/postIndex.mjs";
+import { filterPostsListener } from "./posts/get.mjs";
 
 const path = location.pathname
 
 if (path === '/index.html') {
-    handlers.setLoginFormListener()
+    handlers.setLoginFormListener();
 } else if (path === '/register.html') {
-    handlers.setRegisterFormListener()
+    handlers.setRegisterFormListener();
 } else if (path === '/profile.html') {
-    handlers.setCreatePostListener()
-    postMethods.getMutiplePosts()
-    // handlers.filterPostsListener()
+    handlers.setCreatePostListener();
+    postMethods.getMultiplePosts();
+    postMethods.searchposts();
+    filterPostsListener()
 }  else if (path === '/post.html') {
-    postMethods.getOnePost()
+    postMethods.getOnePost();
 }  else if (path === '/edit_post.html') {
-    handlers.setEditPostListener()
-    handlers.setRemovePostListener()
+    handlers.setEditPostListener();
+    handlers.setRemovePost();
 }
--- js/posts/get.mjs
+++ js/posts/get.mjs
@@ -4,7 +4,7 @@ import { API_POST_AUTHOR } from "/js/api/constants.mjs";
 import { tokenFetch } from "/js/api/tokenFetch.mjs";
 import * as templates from "/js/templates/templateIndex.mjs";
 const action = "/posts";
-
+let posts = [];
 
 // gets single post
 export async function getPost() {
@@ -33,22 +33,82 @@ export async function getOnePost() {
     console.log(post);
 }
 
-
-
 // gets multiple posts
 export async function getPosts() {
     const updatePostURL = `${API_AUTHOR_URL}`;
     const response = await tokenFetch(updatePostURL)
     const post = await response.json();
 
-    // console.log(updatePostURL);
-
     return post;
 }
 
-export async function getMutiplePosts() {
-    const posts = await getPosts()
+  export async function getMultiplePosts() {
+    try {
+      posts = await getPosts();
+      const container = document.querySelector("#news-feed");
+      templates.renderPosts(posts, container);
+      console.log(posts);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+
+// search
+export function searchposts() {
+    const searchInput = document.querySelector("#search-input");
+    const searchQuery = searchInput.value.toLowerCase();
+
+    const filteredPostsByTitle = posts.filter(post => post.title.toLowerCase().includes(searchQuery));
+    const filteredPostsBySeller = posts.filter(post => post.author.name.toLowerCase().includes(searchQuery));
+
+    const filteredPosts = [...filteredPostsByTitle, ...filteredPostsBySeller];
+    console.log(filteredPosts)
+
     const container = document.querySelector("#news-feed");
-    templates.renderPosts(posts, container)
-    console.log(posts);
+    container.innerHTML = "";
+    templates.renderPosts(filteredPosts, container);
 }
+
+const searchButton = document.querySelector("#search-button");
+if (searchButton) {
+  searchButton.addEventListener("click", searchposts);
+}
+
+// filter 
+export function filterPostsListener() {
+    const newest = document.querySelector("#postNew");
+    const oldToNew = document.querySelector("#postOldNew");
+    const withImages = document.querySelector("#postImages");
+  
+    oldToNew.addEventListener("click", (event) => {
+      const oldestToNewest = posts.sort((a, b) => {
+        return new Date(a.created) - new Date(b.created);
+      });
+      const container = document.querySelector("#news-feed");
+      container.innerHTML = "";
+      templates.renderPosts(oldestToNewest, container);
+    });
+  
+    newest.addEventListener("click", (event) => {
+      const newestToOldest = posts.sort((a, b) => {
+        return new Date(b.created) - new Date(a.created);
+      });
+      const container = document.querySelector("#news-feed");
+      container.innerHTML = "";
+      templates.renderPosts(newestToOldest, container);
+    });
+  
+    withImages.addEventListener("click", (event) => {
+      const postsWithImage = posts.filter((post) => {
+        return post.media !== null && post.media !== "";
+      });
+      const container = document.querySelector("#news-feed");
+      container.innerHTML = "";
+      templates.renderPosts(postsWithImage, container);
+    });
+  }
+  
+  
+  
+  
