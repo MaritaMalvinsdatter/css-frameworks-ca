c0c38eb58e5cca19adfc22cde1122b4ab1bf3e88
MaritaMalvinsdatter <79151543+MaritaMalvinsdatter@users.noreply.github.com>
2023-03-13 19:24:32 +0100

    new window location

--- js/auth/register.mjs
+++ js/auth/register.mjs
@@ -3,6 +3,13 @@ import { API_SOCIAL_URL } from "/js/api/constants.mjs";
 const action = "/auth/register";
 const method = "post";
 
+/**
+Sends a user's registration information to the API and returns the API response.
+@async
+@function register
+@param {Object} user - An object containing the user's registration information, including their name, email, and password.
+@returns {Promise<Object>} - A Promise that resolves with the API response as an object.
+*/
 export async function register(user) {
     const registerURL = API_SOCIAL_URL + action;
     const body = JSON.stringify(user);
@@ -18,9 +25,7 @@ export async function register(user) {
 
     const result = await response.json();
     console.log(result);
-    // alert("Registration Complete - Welcome!")
-    // window.location.assign("../profile.html")
-    // document.getElementById("myForm").reset();
+    alert("Registration Complete - Welcome! PLease login")
+    window.location.assign("../index.html")
     return result;
-
 }
