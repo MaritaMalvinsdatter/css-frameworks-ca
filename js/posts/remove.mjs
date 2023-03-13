b6870b23f95c0104a8500eb6a45bc08448bc7fa9
MaritaMalvinsdatter <79151543+MaritaMalvinsdatter@users.noreply.github.com>
2023-03-13 19:26:11 +0100

    setProfile added

--- js/posts/remove.mjs
+++ js/posts/remove.mjs
@@ -5,20 +5,22 @@ import { tokenFetch } from "/js/api/tokenFetch.mjs"
 const action = "/posts"
 
 export async function removePost(id) {
-    if (!id) {
-        throw new Error("Deleting requires a postID");
-    }
-    const removePostURL = `${API_SOCIAL_URL}${action}/${id}${API_POST_AUTHOR}`;
+  if (!id) {
+      throw new Error("Deleting requires a postID");
+  }
+  const removePostURL = `${API_SOCIAL_URL}${action}/${id}${API_POST_AUTHOR}`;
 
-    const response = await tokenFetch(removePostURL, {
-        method: "DELETE",
-    });
+  const response = await tokenFetch(removePostURL, {
+      method: "DELETE",
+  });
 
-    if (response.ok) {
-        alert("Your listing has been deleted!");
-      } else {
-        const errorMessage = `Failed to delete listing with status ${response.status}`;
-        throw new Error(errorMessage);
-      }
+  if (response.ok) {
+      alert("Your listing has been deleted!");
+  } else {
+      const errorMessage = `Failed to delete listing with status ${response.status}`;
+      throw new Error(errorMessage);
+  }
 }
+
+
 
